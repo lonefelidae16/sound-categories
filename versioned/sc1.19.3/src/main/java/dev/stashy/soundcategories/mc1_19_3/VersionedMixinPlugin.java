851925f10974e4b811afa2e9286bc2bb886d5374
lonefelidae16 <kow161206@gmail.com>
2024-08-09 09:33:47 +0900

    [core] Port to 1.21.1
    * update dependencies
      - yarn mappings 1.21.1+build.1
      - fabric api 0.102.0+1.21.1

--- versioned/sc1.19.3/src/main/java/dev/stashy/soundcategories/mc1_19_3/VersionedMixinPlugin.java
+++ versioned/sc1.19.3/src/main/java/dev/stashy/soundcategories/mc1_19_3/VersionedMixinPlugin.java
@@ -10,6 +10,6 @@ public final class VersionedMixinPlugin extends AbstractVersionedMixinPlugin {
 
     @Override
     protected String laterVersion() {
-        return "1.21";
+        return "1.21.1";
     }
 }
